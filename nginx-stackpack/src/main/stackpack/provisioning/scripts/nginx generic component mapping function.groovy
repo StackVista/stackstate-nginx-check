7b3ece4511938b57be50b2c0cf7233d09b23c322
Joris De Winne <joris.dewinne@gmail.com>
2020-06-12 12:00:57 -0700

    Added additional metrics on virtual server status zones

--- nginx-stackpack/src/main/stackpack/provisioning/scripts/nginx generic component mapping function.groovy
+++ nginx-stackpack/src/main/stackpack/provisioning/scripts/nginx generic component mapping function.groovy
@@ -4,13 +4,16 @@ switch (element.type.name) {
     case 'nginx_virtual_server':
         if(element.data.containsKey("status_zone")){
             metric_streams = metric_streams.plus([
-                [ name: "The total number of client requests received from clients.", value: "nginx.server_zone.requests", id: "-1001", aggregation: "MAX"],
-                [ name: "The number of responses with 1xx status codes.", value: "nginx.server_zone.responses.1xx", id: "-1002", aggregation: "MAX"],
-                [ name: "The number of responses with 2xx status codes.", value: "nginx.server_zone.responses.2xx", id: "-1003", aggregation: "MAX"],
-                [ name: "The number of responses with 3xx status codes.", value: "nginx.server_zone.responses.3xx", id: "-1004", aggregation: "MAX"],
-                [ name: "The number of responses with 4xx status codes.", value: "nginx.server_zone.responses.4xx", id: "-1005", aggregation: "MAX"],
-                [ name: "The number of responses with 5xx status codes.", value: "nginx.server_zone.responses.5xx", id: "-1006", aggregation: "MAX"],
-                [ name: "The total number of responses sent to clients.", value: "nginx.server_zone.responses.total", id: "-1007", aggregation: "MAX"]
+                [ name: "The number of client requests that are currently being processed.", value: "nginx.server_zone.processing", id: "-1001", aggregation: "MAX"],
+                [ name: "The total number of client requests received from clients.", value: "nginx.server_zone.requests", id: "-1002", aggregation: "MAX"],
+                [ name: "The number of responses with 1xx status codes.", value: "nginx.server_zone.responses.1xx", id: "-1003", aggregation: "MAX"],
+                [ name: "The number of responses with 2xx status codes.", value: "nginx.server_zone.responses.2xx", id: "-1004", aggregation: "MAX"],
+                [ name: "The number of responses with 3xx status codes.", value: "nginx.server_zone.responses.3xx", id: "-1005", aggregation: "MAX"],
+                [ name: "The number of responses with 4xx status codes.", value: "nginx.server_zone.responses.4xx", id: "-1006", aggregation: "MAX"],
+                [ name: "The number of responses with 5xx status codes.", value: "nginx.server_zone.responses.5xx", id: "-1007", aggregation: "MAX"],
+                [ name: "The total number of responses sent to clients.", value: "nginx.server_zone.responses.total", id: "-1008", aggregation: "MAX"],
+                [ name: "The total number of bytes sent to clients.", value: "nginx.server_zone.sent", id: "-1009", aggregation: "MAX"],
+                [ name: "The total number of bytes received from clients.", value: "nginx.server_zone.received", id: "-1010", aggregation: "MAX"],
             ])
         }
         break
